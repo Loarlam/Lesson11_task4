a791b57db812d786d2e15a52055c61cfad9ef433
Loarlam <messiah02@mail.ru>
2020-04-21 15:29:33 +0300

    Создал класс ArrayList и добавил в него: констуктор по умолчанию; метод добавления  элементов из класса Program; переопределенный метод ToString для вывода элементов на экран. Добавил фоточку!

---
+++ Task4/ArrayList.cs
@@ -0,0 +1,37 @@
+﻿namespace Task4
+{
+    class ArrayList
+    {
+        object[] _objectArray;
+
+        public ArrayList()
+        {
+            _objectArray = new object[0];
+        }
+
+        public void Add(object valueFromClassProgram)
+        {
+           object[] _temporaryObjectArray = new object[_objectArray.Length+1];
+
+            for (int i = 0; i < _objectArray.Length; i++)
+            {
+                _temporaryObjectArray[i] = _objectArray[i];
+            }
+
+            _temporaryObjectArray[_objectArray.Length] = valueFromClassProgram;
+            _objectArray = _temporaryObjectArray;
+        }
+
+        public override string ToString()
+        {
+            string temporaryString = "";
+
+            for (int i = 0; i < _objectArray.Length; i++)
+            {
+                temporaryString += _objectArray[i] + "\n";
+            }
+
+            return $"Массив:\n{temporaryString}";
+        }
+    }
+}
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -9,7 +9,17 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            ArrayList arrayList = new ArrayList();
 
+            arrayList.Add(1);
+            arrayList.Add("QQ, friend");
+            arrayList.Add(4.5);
+            arrayList.Add('Y');
+            arrayList.Add(525252525m);
+
+            Console.WriteLine(arrayList.ToString());
+
+            Console.ReadKey();
         }
     }
 }
