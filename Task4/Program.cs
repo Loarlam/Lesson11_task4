6b7c2f3960298e22c43b399ae3ad9677229ca5b4
Loarlam <messiah02@mail.ru>
2020-04-21 13:17:15 +0300

    Add project files.

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Используя Visual Studio, создайте проект по шаблону Console Application.  
+Создайте класс ArrayList. Реализуйте в простейшем приближении возможность использования его экземпляра аналогично экземпляру класса ArrayList из пространства имен System.Collections. 
+*/
+using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
